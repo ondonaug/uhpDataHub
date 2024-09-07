0512931d1e502d72876172f6516a8970c549d6c5
ondonaug <164528792+ondonaug@users.noreply.github.com>
2024-09-07 18:33:29 +0100

    Update settings.py

--- uhpDataBase/settings.py
+++ uhpDataBase/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_static_jquery',
     'myUHP.apps.MyuhpConfig',
     'crispy_bootstrap4',
     'crispy_forms',
