7f74f5f469c299aa3164c6331f1976287da4c9fd
ondonw <ondona@who.int>
2024-10-04 13:39:26 +0100

    update repository

--- uhpDataBase/settings.py
+++ uhpDataBase/settings.py
@@ -99,10 +99,7 @@ WSGI_APPLICATION = 'uhpDataBase.wsgi.application'
 
 DATABASES = {
     'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-     #   'NAME': BASE_DIR / 'db.sqlite3',
          'ENGINE': 'django.db.backends.mysql',
-       # 'NAME': 'ylpmjzzgdo$uhpcdb-database',
          'NAME': 'uhpdatabase',
          'USER':'ylpmjzzgdo',
          'PASSWORD':'$cGy4BjroDQMMmPn',
